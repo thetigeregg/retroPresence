df3b28f14fbf1be6db24d82677f392f51a88c51e
tigeregg <thetigeregg@gmail.com>
2023-11-10 19:05:46 -0500

    Fix formatting and variable naming in main.py

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import csv
 
-csv_path = './data/games-list.csv'
+csv_path = "./data/games-list.csv"
+
 
 def load_games_list():
     games_dict = {}
-    with open(csv_path, 'r') as file:
+    with open(csv_path, "r") as file:
         reader = csv.DictReader(file)
         for i, row in enumerate(reader):
             if i >= 5:
                 break
-            games_dict[row['name']] = row
+            games_dict[row["name"]] = row
     return games_dict
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     games_dict = load_games_list()
     desired_game_name = input("Please enter a game name: ")
     if desired_game_name in games_dict:
